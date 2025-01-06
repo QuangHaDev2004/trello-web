65432b3bff49e29543bf79491f17c0f8206a1a72
QuangHaDev2004 <ngoquangha2004@gmail.com>
2025-01-06 19:43:11 +0700

    App Bar P1

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,18 +1,58 @@
-import { Box } from "@mui/material";
-import ModeSelect from "../ModelSelect";
-
+import { Badge, Box, Button, TextField, Tooltip, Typography } from "@mui/material"
+import ModeSelect from "../ModelSelect"
+import AppsIcon from '@mui/icons-material/Apps'
+import SvgIcon from '@mui/material/SvgIcon'
+import { ReactComponent as TrelloIcon } from '../../assets/trello.svg';
+import Workspaces from "./Menus/Workspaces";
+import Recent from "./Menus/Recent";
+import Starred from "./Menus/Starred";
+import Templates from "./Menus/Templates";
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import Profiles from "./Menus/Profiles";
 
 function AppBar() {
   return (
     <>
-      <Box sx={{
-        backgroundColor: 'primary.light',
+      <Box px={2} sx={{
         width: '100%',
         height: (theme) => theme.trello.appBarHeight,
         display: 'flex',
-        alignItems: 'center'
+        alignItems: 'center',
+        justifyContent: 'space-between'
       }}>
-        <ModeSelect />
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+          <AppsIcon sx={{ color: 'primary.main' }} />
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
+            <SvgIcon component={TrelloIcon} inheritViewBox sx={{ color: 'primary.main' }} />
+            <Typography variant="span" sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: 'primary.main' }}>
+              Trello
+            </Typography>
+
+            <Workspaces />
+            <Recent />
+            <Starred />
+            <Templates />
+
+            <Button variant="outlined">Create</Button>
+          </Box>
+        </Box>
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+          <TextField id="outlined-search" label="Search..." type="search" size="small" />
+          <ModeSelect />
+
+          <Tooltip title="Notification">
+            <Badge color="secondary" variant="dot" sx={{ cursor: 'pointer' }}>
+              <NotificationsNoneIcon />
+            </Badge>
+          </Tooltip>
+
+          <Tooltip title="Help">
+            <HelpOutlineIcon sx={{ cursor: 'pointer' }} />
+          </Tooltip>
+
+          <Profiles />
+        </Box>
       </Box>
     </>
   )
