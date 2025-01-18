503d585fba1cdcdf68882fe1536a08e573fb0dcc
QuangHaDev2004 <ngoquangha2004@gmail.com>
2025-01-18 10:20:02 +0700

    Drag drop card p1

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,10 +1,19 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import { Box } from "@mui/material";
 import ListColumns from "./ListColumns/ListColumns";
 import { mapOrder } from "../../../utils/sorts";
-import { DndContext, PointerSensor, useSensor, useSensors } from "@dnd-kit/core";
+import { DndContext, PointerSensor, useSensor, useSensors, DragOverlay, defaultDropAnimationSideEffects } from "@dnd-kit/core";
 import { useEffect, useState } from "react";
 import { arrayMove } from "@dnd-kit/sortable";
+
+import Column from "./ListColumns/Column/Column";
+import Card from "./ListColumns/Column/ListCards/Card/Card";
+
+const ACTIVE_DRAG_ITEM_TYPE = {
+  COLUMN: "ACTIVE_DRAG_ITEM_TYPE_COLUMN",
+  CARD: "ACTIVE_DRAG_ITEM_TYPE_CARD"
+}
 
 function BoardContent({ board }) {
   const poiterSensor = useSensor(PointerSensor, { activationConstraint: { distance: 10 } });
@@ -12,10 +21,20 @@ function BoardContent({ board }) {
 
   const [orderedColumnStates, setOrderedColumnStates] = useState([]);
 
+  const [activeDragItemId, setActiveDragItemId] = useState(null);
+  const [activeDragItemType, setActiveDragItemType] = useState(null);
+  const [activeDragItemData, setActiveDragItemData] = useState(null);
+
   useEffect(() => {
     const orderedColumns = mapOrder(board?.columns, board?.columnOrderIds, '_id');
     setOrderedColumnStates(orderedColumns);
   }, [board]);
+
+  const handleDragStart = (event) => {
+    setActiveDragItemId(event?.active?.id);
+    setActiveDragItemType(event?.active?.data?.current?.columnId ? ACTIVE_DRAG_ITEM_TYPE.CARD : ACTIVE_DRAG_ITEM_TYPE.COLUMN);
+    setActiveDragItemData(event?.active?.data?.current);
+  }
 
   const handleDragEnd = (event) => {
     const { active, over } = event;
@@ -31,11 +50,29 @@ function BoardContent({ board }) {
       const dndOrderedColumns = arrayMove(orderedColumnStates, oldIndex, newIndex);
       setOrderedColumnStates(dndOrderedColumns);
     }
+
+    setActiveDragItemId(null);
+    setActiveDragItemType(null);
+    setActiveDragItemData(null);
+  }
+
+  const dropAnimation = {
+    sideEffects: defaultDropAnimationSideEffects({
+      styles: {
+        active: {
+          opacity: "0.5"
+        }
+      }
+    })
   }
 
   return (
     <>
-      <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
+      <DndContext
+        onDragStart={handleDragStart}
+        onDragEnd={handleDragEnd}
+        sensors={sensors}
+      >
         <Box
           sx={{
             backgroundColor: (theme) => theme.palette.mode === "dark" ? "#34495e" : "#1976d2",
@@ -45,6 +82,11 @@ function BoardContent({ board }) {
           }}
         >
           <ListColumns columns={orderedColumnStates} />
+          <DragOverlay dropAnimation={dropAnimation}>
+            {!activeDragItemType && null}
+            {(activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.COLUMN) && <Column column={activeDragItemData} />}
+            {(activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.CARD) && <Card card={activeDragItemData} />}
+          </DragOverlay>
         </Box>
       </DndContext>
     </>
--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
@@ -1,28 +1,34 @@
 /* eslint-disable react/prop-types */
 import { Box } from "@mui/material"
 import Card from "./Card/Card"
+import { verticalListSortingStrategy, SortableContext } from "@dnd-kit/sortable"
 
-function ListCards({cards}) {
+function ListCards({ cards }) {
+  const items = cards?.map(c => c._id);
+
   return (
-    <Box sx={{
-      padding: "0 5px",
-      margin: "0 5px",
-      display: "flex",
-      flexDirection: "column",
-      gap: 1,
-      overflowY: "auto",
-      overflowX: "hidden",
-      maxHeight: (theme) => `calc(${theme.trello.boardContentHeight} - 
+    <SortableContext items={items} strategy={verticalListSortingStrategy}>
+      <Box sx={{
+        padding: "0 5px",
+        margin: "0 5px",
+        display: "flex",
+        flexDirection: "column",
+        gap: 1,
+        overflowY: "auto",
+        overflowX: "hidden",
+        maxHeight: (theme) => `calc(${theme.trello.boardContentHeight} - 
       ${theme.spacing(5)} - 
       ${theme.trello.columnHeaderHeight} - 
       ${theme.trello.columnFooterHeight}) `,
-      '&::-webkit-scrollbar-thumb': { backgroundColor: "#cde0da", },
-      '&::-webkit-scrollbar-thumb:hover': { backgroundColor: "#bfc2cf" }
-    }}>
-      {cards?.map(card => (
-        <Card key={card._id} card={card} />
-      ))}
-    </Box>
+        '&::-webkit-scrollbar-thumb': { backgroundColor: "#cde0da", },
+        '&::-webkit-scrollbar-thumb:hover': { backgroundColor: "#bfc2cf" }
+      }}>
+        {cards?.map(card => (
+          <Card key={card._id} card={card} />
+        ))}
+      </Box>
+    </SortableContext>
+
   )
 }
 
